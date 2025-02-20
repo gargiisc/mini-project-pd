59e66393fcdd7e8f2b7303a0b8cf50df2cd9874a
Gargii <gsc5741@gmail.com>
2025-02-20 09:33:42 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+
+@app.route('/home')
+def home():
+    return "Flask is working! 🚀"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
